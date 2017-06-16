102f09810dc0426901941299533a270001134d9b
cvele <nikola.cvetkovic91@gmail.com>
2017-06-16 19:27:54 +0200

    added body-parser, create-new-camp page and accompanying handlers

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require("express");
 var server = express();
+var bodyParser = require("body-parser");
+
 var campgrounds = [
 		{name: "Salmon Creek", image: "https://source.unsplash.com/mzZVGFfMOkA"},
 		{name: "Granite Hill", image: "https://source.unsplash.com/B9z9TjfIw3I"},
 		{name: "Mountain Goat's Rest", image: "https://source.unsplash.com/oT4hTqWoZ6M"}
 	];
+server.use(bodyParser.urlencoded({extended: true}));
 server.set("view engine", "ejs");
 
 server.get("/", function(req, res){
@@ -13,6 +16,18 @@ server.get("/", function(req, res){
 
 server.get("/campgrounds", function(req, res){
 	res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+server.get("/campgrounds/new", function(req, res){
+	res.render("new.ejs");
+});
+
+server.post("/campgrounds", function(req, res){
+	var name = req.body.name;
+	var image = req.body.image;
+	var newCampground = {name: name, image: image};
+	campgrounds.push(newCampground);
+	res.redirect("/campgrounds");
 });
 
 /*---------------------------*/
