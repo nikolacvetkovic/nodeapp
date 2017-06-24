89471f004eb5bf75521cc7c5106bc22d2dd821ce
cvele <nikola.cvetkovic91@gmail.com>
2017-06-24 13:58:30 +0200

    refactored routes

---
+++ routes/index.js
@@ -0,0 +1,48 @@
+var express = require("express");
+var router = express.Router();
+var User = require("../modules/user");
+var passport = require("passport");
+
+router.get("/", function(req, res){
+	res.render("campgrounds/index");
+});
+
+router.get("/register", function(req, res){
+	res.render("register");
+});
+
+router.post("/register", function(req, res){
+	var newUser = new User({username: req.body.username});
+	User.register(newUser, req.body.password, function(err, user){
+		if(err){
+			return res.render("register");
+		}
+		passport.authenticate("local")(req, res, function(){
+			res.redirect("/campgrounds");
+		});
+	})
+});
+
+router.get("/login", function (req, res) {
+	res.render("login");
+});
+
+router.post("/login", passport.authenticate("local", {
+	successRedirect: "/campgrounds",
+	failureRedirect: "/login"
+}), function(req, res){
+});
+
+router.get("/logout", function(req, res){
+	req.logout();
+	res.redirect("/");
+});
+
+function isLoggedIn(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	}
+	res.redirect("/login");
+}
+
+module.exports = router;
